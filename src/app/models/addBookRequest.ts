9fd4d6065c09342ae52fd7b48da5ef0f89cdf5be
Sercan Ateş <sercanio@users.noreply.github.com>
2024-03-15 15:35:52 +0300

    Create project

---
+++ src/app/models/addBookRequest.ts
@@ -0,0 +1,8 @@
+// CTRL + ALT + v -> json2ts
+// Copy JSON response and copy with the shortcut it will be a typescript interface
+
+// Sample Code
+export interface AddBookRequest {
+  title: string;
+  author: string;
+}
