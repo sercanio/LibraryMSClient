e999bf57de60b156de33323c6ecfe989ee721ba9
Sercan Ateş <sercanio@users.noreply.github.com>
2024-03-23 18:29:12 +0300

    Add contact page

---
+++ src/app/pages/contact/contact.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterModule],
+  templateUrl: './contact.component.html',
+  styleUrl: './contact.component.scss'
+})
+export class ContactComponent {
+
+}
