047e89db5ff9f8525d9f88217794ce8b192bc230
Sercan Ateş <sercanio@users.noreply.github.com>
2024-03-24 21:23:16 +0300

    Change old Twitter icon into X icon

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,13 +1,17 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { featherFacebook, featherTwitter } from '@ng-icons/feather-icons';
+import { bootstrapTwitterX } from '@ng-icons/bootstrap-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [CommonModule, RouterModule],
+  imports: [CommonModule, RouterModule, NgIconComponent],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss',
+  viewProviders: [provideIcons({ featherFacebook, bootstrapTwitterX })],
 })
 export class HeaderComponent {
   isMenuOpen: boolean = false;
