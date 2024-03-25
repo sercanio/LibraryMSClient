778d83483cb80054305a16c41b973eba5d1b94a8
Sercan Ateş <sercanio@users.noreply.github.com>
2024-03-25 11:53:21 +0300

    Extend tailwind theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,15 +15,32 @@ module.exports = {
         primary: '#F2F0EE',
         secondary: '#1B1B1B',
         tertiary: '##EAE7E4',
+        "primary-button": '#F3701D',
+      },
+      borderColor: {
+        primary: '#F3701D',
+        secondary: '#E23826',
+        tertiary: '#0000FF',
       },
       fontFamily: {
         sans: ['Arial', 'Helvetica', 'sans-serif', 'system-ui', 'BlinkMacSystemFont', '-apple-system'],
-      }
+      },
+      animation: {
+        fade: 'fadeIn .25s ease-in-out',
+      },
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { opacity: theme('opacity.90') },
+          '100%': { opacity: theme('opacity.100') },
+        },
+      }),
     },
   },
-  plugins: [require("daisyui")],
+  plugins: [require("daisyui"), require("tw-elements/plugin.cjs")],
   daisyUi: {
-    themes: ["light", "dark"],
+    themes: [
+      "light", "dark"
+    ],
     darkTheme: "dark",
     base: true,
     utils: true,
@@ -31,4 +48,3 @@ module.exports = {
     styled: true,
   }
 }
-
